616fc42890c3c5ffb78cb58fb1fbec5b59419364
Adam Stevenson <amstevenson12@gmail.com>
2022-12-12 18:18:31 -0500

    Updated class to named export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export default class BetterMutationObserver extends MutationObserver {
+export class BetterMutationObserver extends MutationObserver {
   target: Node | Array<Node> | NodeList
   options: MutationObserverInit
 
